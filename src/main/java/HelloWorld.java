441a77d95a50c552e3b8c8978508694d5a8a9746
Jason Harrop <jason@plutext.org>
2019-03-25 10:48:03 +1100

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,24 @@
+import org.docx4j.XmlUtils;
+import org.docx4j.jaxb.Context;
+import org.docx4j.wml.P;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class HelloWorld implements BundleActivator {
+	
+    public void start(BundleContext ctx) {
+        System.out.println("Hello world.");
+        
+        try {
+	        P p = Context.getWmlObjectFactory().createP();
+	        System.out.println(XmlUtils.marshaltoString(p));
+	        System.out.println("it worked.");
+        } catch (Throwable e) {
+        	e.printStackTrace();
+        }
+        
+    }
+    public void stop(BundleContext bundleContext) {
+        System.out.println("Goodbye world.");
+    }
+}	
