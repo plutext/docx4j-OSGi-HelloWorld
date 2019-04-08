ffcb59955f0edbcdcd201679fb5e7ea40c202960
Jason Harrop <jason@plutext.org>
2019-04-08 15:57:42 +1000

    use with docx4j 8

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 import org.docx4j.XmlUtils;
 import org.docx4j.jaxb.Context;
+import org.docx4j.jaxb.NamespacePrefixMapperUtils;
 import org.docx4j.wml.P;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -11,6 +12,7 @@ public class HelloWorld implements BundleActivator {
         
         try {
 	        P p = Context.getWmlObjectFactory().createP();
+	        System.out.println("NamespacePrefixMapper: " + NamespacePrefixMapperUtils.getPrefixMapper().getClass().getName());
 	        System.out.println(XmlUtils.marshaltoString(p));
 	        System.out.println("it worked.");
         } catch (Throwable e) {
